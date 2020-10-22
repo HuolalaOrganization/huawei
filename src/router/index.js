00245595a6e7f770c0b1b3d4f933aeab877b0626
e <1414237158@qq.com>
2020-10-22 18:18:03 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import homePage from '../page/homePage.vue'
+import detailsPage from '../page/detailsPage.vue'
+import makeGroup from '../page/makeGroup.vue'
+import Dataifon from '../components/Dataifon.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'homePage',
+    component: homePage
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/homePage',
+    name: 'homePage',
+    component: homePage
+  },
+  {
+    path: '/detailsPage/:id',
+    name: 'detailsPage',
+    component: detailsPage,
+    props:true
+  },
+  {
+    path: '/makeGroup',
+    name: 'makeGroup',
+    component: makeGroup
+  },
+  {
+    path: '/Dataifon',
+    name: 'Dataifon',
+    component: Dataifon
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
